bed73b6946bab33599754baa75aa380ceca37eb2
nairod66 <soixante6dollars@gmail.com>
2016-12-05 16:07:03 -0500

    Ajout table Annonce et cellulaire+automobile

--- kijiji2.sql
+++ kijiji2.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Mer 30 Novembre 2016 à 23:17
+-- Généré le :  Lun 05 Décembre 2016 à 21:05
 -- Version du serveur :  5.7.11
 -- Version de PHP :  5.6.18
 
@@ -23,6 +23,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `annonce`
+--
+
+CREATE TABLE `annonce` (
+  `id` int(11) NOT NULL,
+  `titre` varchar(50) NOT NULL,
+  `description` text,
+  `typeObjet` varchar(25) DEFAULT NULL,
+  `adresse_ville` varchar(30) NOT NULL,
+  `adresse_codePostal` varchar(10) NOT NULL,
+  `adresse_province` varchar(30) NOT NULL,
+  `adresse_pays` varchar(30) NOT NULL,
+  `etatObjet` varchar(25) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `idMembre` int(11) NOT NULL,
+  `etat` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `membre`
 --
 
@@ -33,14 +54,14 @@ CREATE TABLE `membre` (
   `nom` varchar(30) NOT NULL,
   `prenom` varchar(30) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `avatar` longblob NOT NULL,
-  `adresse_numero` varchar(25) NOT NULL,
-  `adresse_rue1` varchar(25) NOT NULL,
-  `adresse_rue2` varchar(25) NOT NULL,
-  `adresse_appartement` varchar(25) NOT NULL,
-  `adresse_ville` varchar(25) NOT NULL,
-  `adresse_codePostal` varchar(25) NOT NULL,
-  `adresse_province` varchar(25) NOT NULL,
+  `avatar` longblob,
+  `adresse_numero` varchar(25) DEFAULT NULL,
+  `adresse_rue1` varchar(25) DEFAULT NULL,
+  `adresse_rue2` varchar(25) DEFAULT NULL,
+  `adresse_appartement` varchar(25) DEFAULT NULL,
+  `adresse_ville` varchar(25) DEFAULT NULL,
+  `adresse_codePostal` varchar(25) DEFAULT NULL,
+  `adresse_province` varchar(25) DEFAULT NULL,
   `adresse_pays` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -49,11 +70,58 @@ CREATE TABLE `membre` (
 --
 
 INSERT INTO `membre` (`id`, `username`, `password`, `nom`, `prenom`, `email`, `avatar`, `adresse_numero`, `adresse_rue1`, `adresse_rue2`, `adresse_appartement`, `adresse_ville`, `adresse_codePostal`, `adresse_province`, `adresse_pays`) VALUES
-(23, '', '', 'Smith', 'John', '', '', '', '', '', '', '', '', '', 'Canada');
+(23, '', '', 'Smith', 'John', '', '', '', '', '', '', '', '', '', 'Canada'),
+(24, 'Dodo', 'dodo', 'Dorian', 'Bourcet', 'dodo@dodo.com', '', '5235', 'rue Desmarteau', '', '', 'Montréal', 'H1K 2N7', 'Québec', 'Canada'),
+(25, 'Toto', 'toto', 'tototo', 'to', '', '', '', '', '', '', '', '', '', 'Canada');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `type_automobile`
+--
+
+CREATE TABLE `type_automobile` (
+  `idAnnonce` int(11) NOT NULL,
+  `marque` varchar(25) NOT NULL,
+  `modele` varchar(25) NOT NULL,
+  `puissance` int(11) NOT NULL,
+  `annee` int(11) NOT NULL,
+  `carburant` varchar(25) NOT NULL,
+  `nombrePortes` int(11) NOT NULL,
+  `couleur` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `type_cellulaire`
+--
+
+CREATE TABLE `type_cellulaire` (
+  `idAnnonce` int(11) NOT NULL,
+  `marque` varchar(25) NOT NULL,
+  `nomCommercial` varchar(25) NOT NULL,
+  `modele` varchar(25) NOT NULL,
+  `capaciteStockage` int(11) NOT NULL,
+  `stockageExterne` varchar(25) NOT NULL,
+  `typeReseau` varchar(25) NOT NULL,
+  `Couleur` varchar(25) NOT NULL,
+  `systemeExploitation` varchar(25) NOT NULL,
+  `typeEcran` varchar(25) NOT NULL,
+  `tailleEcran` int(11) NOT NULL,
+  `typeBatterie` varchar(25) NOT NULL,
+  `capaciteBatterie` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Index pour les tables exportées
 --
+
+--
+-- Index pour la table `annonce`
+--
+ALTER TABLE `annonce`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `membre`
@@ -67,10 +135,15 @@ ALTER TABLE `membre`
 --
 
 --
+-- AUTO_INCREMENT pour la table `annonce`
+--
+ALTER TABLE `annonce`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT pour la table `membre`
 --
 ALTER TABLE `membre`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
